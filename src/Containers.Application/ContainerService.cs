f31a5f632425c3e76179d4e589f9f27a64f1f211
danylooliinyk <danylooliinyk.dev@gmail.com>
2025-04-18 11:20:10 +0200

    ContainerService

--- src/Containers.Application/ContainerService.cs
+++ src/Containers.Application/ContainerService.cs
@@ -1,6 +1,50 @@
-﻿namespace Containers.Application;
+﻿using Containers.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Containers.Application;
 
 public class ContainerService
 {
+    private string _connectionString;
+    public ContainerService(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    public IEnumerable<Container> GetAllContainers()
+    {
+        List<Container> containers = [];
+        const string queryString = "SELECT * FROM Containers";
+
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            SqlCommand command = new SqlCommand(queryString, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            try
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        var containerRow = new Container
+                        {
+                            ID = reader.GetInt32(0),
+                            ContainerTypeID = reader.GetInt32(1),
+                            IsHazaerdous = reader.GetBoolean(2),
+                            ContainerName = reader.GetString(3)
+                        };
+                        containers.Add(containerRow);
+                    }
+                }
+            }
+            finally
+            {
+                reader.Close();
+            }
+            
+            return containers;
+        }
+    }
     
 }
