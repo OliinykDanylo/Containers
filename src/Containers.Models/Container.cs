cec7770b26329785b6cdda21e897700310d6e727
danylooliinyk <danylooliinyk.dev@gmail.com>
2025-04-18 11:05:16 +0200

    commit

--- src/Containers.Models/Container.cs
+++ src/Containers.Models/Container.cs
@@ -2,5 +2,8 @@
 
 public class Container
 {
-    
+    public int ID { get; set; }
+    public int ContainerTypeID { get; set; }
+    public bool IsHazaerdous { get; set; }
+    public string ContainerName { get; set; }
 }
